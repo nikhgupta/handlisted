d0fab93679dba289945d85a62ad0e9b0d4b64b9e
Nikhil Gupta <me@nikhgupta.com>
2015-07-25 08:22:16 +0530

    fixed :flipkart factory where model was not initialized as expected

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -2,10 +2,10 @@ FactoryGirl.define do
   factory :merchant do
     sequence(:name){ |n| "Merchant #{n}" }
     service "Merchant X's Priority Service"
+  end
 
-    factory :flipkart do
-      service 'Flipkart Advantage'
-      initialize_with { Merchant.find_or_initialize_by(name: "Flipkart!") }
-    end
+  factory :flipkart, class: Merchant do
+    service 'Flipkart Advantage'
+    initialize_with { Merchant.find_or_initialize_by(name: "Flipkart!") }
   end
 end
