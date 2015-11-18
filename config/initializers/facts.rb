69f3521d95858aacb1df85eb805d69fa30509db8
Nikhil Gupta <me@nikhgupta.com>
2015-11-18 19:53:29 +0530

    updated alpha starting date

--- config/initializers/facts.rb
+++ config/initializers/facts.rb
@@ -4,7 +4,7 @@ module HandListed
     DEVELOPER_EMAILS = ["me@nikhgupta.com"]
 
     # important dates
-    ALPHA_STARTED_ON = Time.parse "Nov 17, 2015"
+    ALPHA_STARTED_ON = Time.parse "Nov 18, 2015"
     BETA_STARTED_ON  = Time.parse "Jan 01, 2016"
     LAUNCHED_ON      = Time.parse "Apr 01, 2016"
   end
